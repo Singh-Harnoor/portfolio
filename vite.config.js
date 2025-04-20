a52b9d3a057f89b498797e2ed48d0714beada3f2
Harnoor Singh <harnoorsingh@Harnoors-MacBook-Pro.local>
2025-04-20 15:15:45 -0400

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: 'portfolio', // 👈 Required for GitHub Pages
+  base: '/portfolio/', // ✅ Make sure it starts and ends with a slash
   plugins: [vue()],
+  assetsInclude: ['**/*.3gp'], // ✅ Let Vite know you’re including .3gp files
 })
