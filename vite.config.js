2694740080a006cf80aa150b9eef811dd4db38e3
Harnoor Singh <harnoorsingh@Harnoors-MacBook-Pro.local>
2025-12-02 18:45:07 -0500

    Deploy portfolio

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: './', // relative paths work when serving from root
+  base: './',   // ✅ relative paths for main branch root
   plugins: [vue()],
-  assetsInclude: ['**/*.3gp'],
 })
